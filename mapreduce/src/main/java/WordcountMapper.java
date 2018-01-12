2d06b6ae932ff6a8e4a66aa9fdd132dc9374df1a
JeremyLeeY <453323744@qq.com>
2018-01-12 17:17:53 +0800

    hadoop wordcount code

---
+++ mapreduce/src/main/java/WordcountMapper.java
@@ -0,0 +1,18 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordcountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+        String line = value.toString();
+
+        String[] words = line.split(",");
+
+        for (String word: words) {
+            context.write(new Text(word), new IntWritable(1));
+        }
+    }
+}
