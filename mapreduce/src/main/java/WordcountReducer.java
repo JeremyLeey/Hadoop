9e76a7c254f97e8cccd58a5a0518c75b5cacb6a9
JeremyLeeY <453323744@qq.com>
2018-01-12 18:06:29 +0800

    Revert "Revert "hadoop wordcount code""
    This reverts commit 10f89276979a738d5a5f33575fac20ce8761cd07.

---
+++ mapreduce/src/main/java/WordcountReducer.java
@@ -0,0 +1,15 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordcountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+        Integer count = 0;
+        for (IntWritable value: values) {
+            count += value.get();
+        }
+        context.write(key, new IntWritable(count));
+    }
+}
